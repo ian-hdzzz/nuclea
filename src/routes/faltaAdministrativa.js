88235ce6a94932e30027b197722a871837216f2e
Ian <ianhdez2020@gmail.com>
2025-03-06 18:22:22 -0600

    ruta de faltas creada

---
+++ src/routes/faltaAdministrativa.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+// Definir rutas
+router.get('/faltasAdministrativas', (req, res) => {
+  res.render('./pages/faltasAdministrativas');
+});
+
+
+module.exports =  router;
