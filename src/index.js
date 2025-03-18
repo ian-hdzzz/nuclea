29c31f128fd770b680f53e71ca23358cdc9da672
SebastianOso <sebastian.oso03@gmail.com>
2025-03-18 17:46:44 -0600

    agregar express-session para que funcione lib csurf

--- src/index.js
+++ src/index.js
@@ -32,6 +32,13 @@ app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 app.use(express.static('public'));
 
+const session = require('express-session');
+app.use(session({
+    secret: 'mi string secreto que debe ser un string aleatorio muy largo, no como éste', 
+    resave: false, //La sesión no se guardará en cada petición, sino sólo se guardará si algo cambió 
+    saveUninitialized: false, //Asegura que no se guarde una sesión para una petición que no lo necesita
+}));
+
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));
 
