35884b0914c25b92dcc3c0310bc488b1d9dca37f
Angel D CR <Angel>
2025-03-25 13:41:14 -0600

    Interfaz basica de Request

--- src/controllers/request.js
+++ src/controllers/request.js
@@ -1,29 +1,15 @@
-const Departament = require('../models/request.model');
+const Request = require('../models/request.model');
 
-exports.getDepartaments = (req, res) => {
-    Departament.fetchAll()
-      .then(([rows, fieldData]) => {
-        res.render('../views/pages/request.hbs', { 
-            datos: rows,
-            csrfToken: req.csrfToken(),
-        });
-      })
-      .catch((err) => {
-        console.error('Error fetching departments:', err);
-        res.status(500).send('Internal Server Error');
+exports.getRequests = (req, res) => {
+  Request.fetchAll()
+    .then(([rows]) => {
+      res.render('pages/request', {
+        datos: rows,
+        csrfToken: req.csrfToken()
       });
-  };
-
-  exports.post_agregar_dep = (request, response, next) => {
-    console.log(request.body);
-    console.log(request.body.fecha);
-    const falta = new Departament(request.body.Nombre_departamento, request.body.Descripcion, request.body.Estado);
-    falta.save()
-        .then(() => {
-            response.redirect('/nuclea/request');
-        })
-        .catch((error) => {
-            console.log(error);
-        });
+    })
+    .catch((err) => {
+      console.error('Error al cargar las solicitudes:', err);
+      res.status(500).send('Error al obtener los datos');
+    });
 };
-
