5be3e41dbabe1afe3610c5c2be92b8a94ea298cc
SebastianOso <sebastian.oso03@gmail.com>
2025-03-26 14:22:41 -0600

    add message to know there is no data in the table

--- src/controllers/faltas.controller.js
+++ src/controllers/faltas.controller.js
@@ -1,25 +1,30 @@
-//faltas controller
 const Falta = require('../models/faltas.model');
 const Usuario = require('../models/usuario.model');
 
-
 exports.get_fa = (req, res, next) => {
+    Falta.fetchFA()
+        .then(([faltas, fD]) => {
+            Usuario.fetchAll()
+                .then(([rows, fieldData]) => {
+                    // Check if faltas is empty and set a flag
+                    const noFaltas = faltas.length === 0;
 
-    Falta.fetchFA().then(([faltas, fD])=>{
-        Usuario.fetchAll().then(([rows, fieldData])=>{
-            res.render('../views/pages/faltasAdministrativas.hbs', {
-                usuariosfa:rows,
-                csrfToken: req.csrfToken(),
-                faltas: faltas
-            });
-        }).catch((err)=>{
-            console.error('Error fetching Users:', err);
+                    res.render('../views/pages/faltasAdministrativas.hbs', {
+                        usuariosfa: rows,
+                        csrfToken: req.csrfToken(),
+                        faltas: faltas,
+                        noFaltas: noFaltas // Pass the flag to the template
+                    });
+                })
+                .catch((err) => {
+                    console.error('Error fetching Users:', err);
+                    res.status(500).send('Internal Server Error');
+                });
+        })
+        .catch((err) => {
+            console.error('Error fetching Administrative offenses:', err);
             res.status(500).send('Internal Server Error');
         });
-    }).catch((err)=>{
-        console.error('Error fetching Administrative ofenses:', err);
-        res.status(500).send('Internal Server Error');
-    })
 };
 
 exports.post_agregar_fa = (req, res, next) => {
@@ -27,19 +32,18 @@ exports.post_agregar_fa = (req, res, next) => {
     const archivo = req.file ? req.file.filename : null;
 
     const falta = new Falta(
-      req.body.idUsu,
-      req.body.fecha,
-      req.body.motivo,
-      archivo
+        req.body.idUsu,
+        req.body.fecha,
+        req.body.motivo,
+        archivo
     );
 
     falta.save()
-      .then(() => {
-        res.redirect('/nuclea/faltasAdministrativas');
-      })
-      .catch((error) => {
-        console.error("Error al asignar falta:", error);
-        res.status(500).send('Error registrando falta administrativa');
-      });
+        .then(() => {
+            res.redirect('/nuclea/faltasAdministrativas');
+        })
+        .catch((error) => {
+            console.error("Error al asignar falta:", error);
+            res.status(500).send('Error registrando falta administrativa');
+        });
 };
-
