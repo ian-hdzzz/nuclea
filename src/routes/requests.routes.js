7ef9474ce357c679f1cb471d678f21e881508847
Elmay05 <fewdinosaur@gmail.com>
2025-03-27 13:48:45 -0600

    Aunth en req

--- src/routes/requests.routes.js
+++ src/routes/requests.routes.js
@@ -1,8 +1,9 @@
+const isAuth = require('../util/is-auth')
 const express = require('express');
 const router = express.Router();
 const requestController = require('../controllers/request');
 
 // Ruta GET para mostrar solicitudes
-router.get('/request', requestController.getRequests);
-router.post('/request', requestController.postRequest);
+router.get('/request', isAuth,requestController.getRequests);
+router.post('/request',isAuth, requestController.postRequest);
 module.exports = router;
