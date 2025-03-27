eb3ad032fe3c8c4aadc5a498a2c84bd7a95e4f73
Angel D CR <Angel>
2025-03-27 13:49:20 -0600

    Implementar Holidays y correción routes

---
+++ src/models/diasferiados.model.js
@@ -0,0 +1,21 @@
+const db = require('../util/database');
+
+module.exports = class DiasFeriados {
+
+  constructor(Id, Tipo, Fecha_I, Fecha_F,Descripcion) {
+    this.id = Id;
+    this.tipo = Tipo;
+    this.fecha_i= Fecha_I;
+    this.fecha_f= Fecha_F;
+    this.descripcion = Descripcion;
+}
+
+  // Método para obtener todas las solicitudes
+  static fetchAll() {
+    return db.execute(`
+      SELECT Nombre_asueto, Fecha_asueto
+      FROM DiasFeriados;
+    `);
+    
+  }
+};
