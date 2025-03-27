ac0fe41d0977bee94b04f74fc1a65c67390c489a
Elmay05 <fewdinosaur@gmail.com>
2025-03-27 11:12:18 -0600

    Cambios en perfil

--- src/controllers/profile.controller.js
+++ src/controllers/profile.controller.js
@@ -1,5 +1,6 @@
 exports.get_profile = (req, res, next)=>{
     res.render('./pages/profile', {
+        id: req.session.idUsuario || [],
         nombre: req.session.nombre || [],
         apellidos: req.session.apellidos || [],
         email: req.session.email || [],
