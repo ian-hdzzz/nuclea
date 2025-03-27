ef263002b9d1a2b1e3057059a51066b5f2337e69
Angel D CR <Angel>
2025-03-26 19:19:24 -0600

    Guardado de cambios incompletos para implementar Nombre y Apellidos en formulario

--- src/models/request.model.js
+++ src/models/request.model.js
@@ -5,17 +5,20 @@ module.exports = class Request {
   static fetchAll() {
     return db.execute(`
       SELECT 
-        idSolicitud,
-        idUsuario,
-        Tipo,
-        Fecha_inicio,
-        Fecha_fin,
-        Descripcion,
-        Aprobacion_L,
-        Fecha_aprob_L,
-        Aprobacion_A,
-        Fecha_aprob_A
-      FROM Solicitudes
+        s.idSolicitud,
+        u.Nombre,
+        u.Apellidos AS Apellido,
+        s.Tipo,
+        s.Fecha_inicio,
+        s.Fecha_fin,
+        s.Descripcion,
+        s.Aprobacion_L,
+        s.Fecha_aprob_L,
+        s.Aprobacion_A,
+        s.Fecha_aprob_A
+      FROM Solicitudes s
+      JOIN Usuarios u ON s.idUsuario = u.idUsuario
     `);
+    
   }
 };
