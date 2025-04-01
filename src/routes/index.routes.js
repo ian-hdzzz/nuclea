74234b389f3044fe3d3278b75838e7767b845599
Elmay05 <fewdinosaur@gmail.com>
2025-04-01 09:39:01 -0600

    Descomentando ruta principal

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -3,9 +3,9 @@ const isAuth = require('../util/is-auth');
 const router = express.Router();
 
 // Definir rutas
-// router.get('/', isAuth,(req, res) => {
-//   res.redirect('/')
-// });
+router.get('/', isAuth,(req, res) => {
+  res.redirect('/');
+});
 
 
 module.exports =  router;
