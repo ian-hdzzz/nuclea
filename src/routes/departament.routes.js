51b2a2ed591341c691f74072cc0d0fa0616c11fb
SebastianOso <sebastian.oso03@gmail.com>
2025-04-02 12:03:36 -0600

    edit delete departamento

--- src/routes/departament.routes.js
+++ src/routes/departament.routes.js
@@ -5,7 +5,7 @@ const express = require('express');
  
  // Definir rutas
  router.get('/',isAuth,departamentController.getDepartaments);
- router.post('/',isAuth,departamentController.post_agregar_dep)
+ router.post('/',isAuth,departamentController.post_agregar_dep);
  router.get('/delete/:idDepartamento',departamentController.get_delete);
  router.get('/update/:idDepartamento',departamentController.get_update)
 router.post('/update/:idDepartamento',departamentController.post_update)
