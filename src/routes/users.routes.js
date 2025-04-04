6f755072822ca5ea1d78452c6299b6bb8c74ff58
Jacob Espejel Guiza <A01712021@tec.mx>
2025-04-03 23:22:56 -0600

    Añadiendo Funcionalidad de Admin consulta Usuario

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -8,5 +8,6 @@ router.get('/',isAuth,users_controller.get_users)
 router.post('/',isAuth,users_controller.post_users)
 router.get('/logout',users_controller.get_logout)
 router.get('/delete/:idUsuario',users_controller.get_delete);
+router.get('/view/:idUsuario',users_controller.get_view);
 
 module.exports =  router;  
