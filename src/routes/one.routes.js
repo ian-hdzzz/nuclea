929d3b4f71277b740f6d1b8669b429402d4d2390
Ian <ianhdez2020@gmail.com>
2025-04-04 12:24:02 -0600

    seccion de satisfaccion de colaboradores y grafico de araño listo

--- src/routes/one.routes.js
+++ src/routes/one.routes.js
@@ -3,7 +3,8 @@ const OneToOneController = require('../controllers/oneToOne.controller');
 const isAuth = require('../util/is-auth')
 const router = express.Router();
 
-router.get('/interview', OneToOneController.getInterview);
+router.get('/one', OneToOneController.getOneToOne);
+
 // router.post('/one/submit', OneToOneController.submitOneToOneInterview);
 
 module.exports = router;
