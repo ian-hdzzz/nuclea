65f2bef26997995b838c71360956d6634656a43d
SebastianOso <sebastian.oso03@gmail.com>
2025-04-06 16:13:40 -0600

    fix company.model

--- src/models/empresa.model.js
+++ src/models/empresa.model.js
@@ -1,26 +1,35 @@
 // eslint-disable-next-line no-undef
 const db = require('../util/database');
- 
+
 // eslint-disable-next-line no-undef
 module.exports = class Empresa {
+  /**
+   * Constructor de la clase. Sirve para crear un nuevo objeto,
+   * y en él se definen las propiedades del modelo.
+   * @param {string} nombre - Nombre de la empresa
+   * @param {boolean|number} estado - Estado de la empresa
+   */
+  constructor(nombre, estado) {
+    this.nombre = nombre;
+    this.estado = estado;
+  }
 
-    //Constructor de la clase. Sirve para crear un nuevo objeto, y en él se definen las propiedades del modelo
-    constructor(nombre,estado) {
-        this.nombre = nombre;
-        this.estado = estado;
-    }
+  /**
+   * Este método servirá para guardar de manera persistente el nuevo objeto.
+   * @return {Promise} Resultado de la consulta
+   */
+  save() {
+    return db.execute(
+        'INSERT INTO Empresa (Nombre_empresa, Estado) VALUES (?, ?)',
+        [this.nombre, this.estado]
+    );
+  }
 
-    //Este método servirá para guardar de manera persistente el nuevo objeto. 
-    save() {
-        return db.execute(
-            'INSERT INTO Empresa (Nombre_empresa, Estado) VALUES (?, ?)',
-            [this.nombre, this.estado]
-        );
-        
-    }
-
-    //Este método servirá para devolver los objetos del almacenamiento persistente.
-    static fetchAll() {
-        return db.execute('SELECT * FROM Empresa');
-    }
-}
+  /**
+   * Este método servirá para devolver los objetos del almacenamiento persistente.
+   * @return {Promise} Resultado de la consulta con todas las empresas
+   */
+  static fetchAll() {
+    return db.execute('SELECT * FROM Empresa');
+  }
+};
