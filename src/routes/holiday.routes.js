10a163edd9d72d29e9038d1bdc8d66037a25316b
Jacob Espejel Guiza <A01712021@tec.mx>
2025-04-05 19:59:02 -0600

    Añadiendo de nuevo protección de rutas

--- src/routes/holiday.routes.js
+++ src/routes/holiday.routes.js
@@ -5,10 +5,10 @@ const holidayController = require('../controllers/holiday.controller')
 const canviewAdmin = require('../util/canviewAdmin');
  
 // Definir rutas
-router.get('/', holidayController.get_Holiday);
-router.post('/', holidayController.post_agregar_holiday)
+router.get('/', isAuth,holidayController.get_Holiday);
+router.post('/', isAuth,holidayController.post_agregar_holiday)
 //router.delete('/delete/:idDepartamento',departamentController.get_delete);
 //router.get('/update/:idDepartamento',departamentController.get_update)
 //router.post('/update/:idDepartamento',departamentController.post_update)
-router.get('/search', holidayController.get_search_holiday);
+router.get('/search', isAuth,holidayController.get_search_holiday);
 module.exports =  router;
