79d1e344ad50f2b34ee74e56fdc22a228302d5fd
SebastianOso <sebastian.oso03@gmail.com>
2025-04-07 09:24:20 -0600

    change holiday js

--- src/public/js/holidays.js
+++ src/public/js/holidays.js
@@ -1,3 +1,24 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const openModal = document.getElementById("openModal");
+    const closeModal = document.getElementById("closeModal");
+    const modal = document.getElementById("modal");
+
+    openModal.addEventListener("click", () => {
+        modal.classList.remove("hidden");
+    });
+
+    closeModal.addEventListener("click", () => {
+        modal.classList.add("hidden");
+    });
+
+    // Cierra el modal si se hace clic fuera de él
+    modal.addEventListener("click", (e) => {
+        if (e.target === modal) {
+            modal.classList.add("hidden");
+        }
+    });
+});
+
 const btn_buscar = document.getElementById("search-input");
 
 btn_buscar.addEventListener("keyup", () => {
