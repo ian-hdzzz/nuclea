1f26a6595423a0646fe2ac38d5f681ee0bb25d0c
Elmay05 <fewdinosaur@gmail.com>
2025-04-07 23:04:07 -0600

    Consultar vacaciones en Dashboard

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -1,8 +1,12 @@
-
+const Request = require('../models/request.model');
 exports.getDashboard = (req, res) => {
-    res.render('pages/dashboard',{
-        title:'Dashboard', 
-        iconClass:'fa-solid fa-house', 
-        currentPath: req.path 
-    });
+    Request.fetchDays(req.session.idUsuario)
+    .then(([rows])=>
+        res.render('pages/dashboard',{
+            title:'Dashboard', 
+            iconClass:'fa-solid fa-house', 
+            currentPath: req.path,
+            dias_restantes: rows[0].dias_vaciones
+        }));
+
 };
