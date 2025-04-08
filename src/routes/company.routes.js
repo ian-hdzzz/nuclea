785fbf4bd35ad31f702fbe1629b1bc2805ea41b2
SebastianOso <sebastian.oso03@gmail.com>
2025-04-08 14:17:14 -0600

    update routes

--- src/routes/company.routes.js
+++ src/routes/company.routes.js
@@ -8,5 +8,7 @@ const canviewAdmin = require('../util/canviewAdmin');
 router.get('/', isAuth, canviewAdmin, companyController.get_company);
 router.post('/', isAuth, canviewAdmin, companyController.post_agregar_company);
 router.get('/delete/:idEmpresa', isAuth, canviewAdmin, companyController.get_delete);
+router.get('/update/:idFalta',companyController.get_update)
+router.post('/update/:idFalta',companyController.post_update)
 
 module.exports = router;
