93b4b7d434da5a9fb6008df88c014672196c5c7a
SebastianOso <sebastian.oso03@gmail.com>
2025-04-09 14:20:23 -0600

    add update routes to company routes

--- src/routes/company.routes.js
+++ src/routes/company.routes.js
@@ -8,7 +8,7 @@ const canviewAdmin = require('../util/canviewAdmin');
 router.get('/', isAuth, canviewAdmin, companyController.get_company);
 router.post('/', isAuth, canviewAdmin, companyController.post_agregar_company);
 router.get('/delete/:idEmpresa', isAuth, canviewAdmin, companyController.get_delete);
-router.get('/update/:idFalta',companyController.get_update)
-router.post('/update/:idFalta',companyController.post_update)
+router.get('/update/:idEmpresa',companyController.get_update)
+router.post('/update/:idEmpresa',companyController.post_update)
 
 module.exports = router;
