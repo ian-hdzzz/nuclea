b5cc858522ce4240aa8166bc7b599de535fc1e81
Jacob Espejel Guiza <A01712021@tec.mx>
2025-04-10 08:57:44 -0600

    Resolviendo conflictos y añadiendo routes de users en develop

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -5,16 +5,6 @@ const isAuth = require('../util/is-auth')
 const canviewAdmin = require('../util/canviewAdmin');
 
 // Definir rutas
-<<<<<<< HEAD
-router.get('/',isAuth,users_controller.get_users)
-router.post('/',isAuth,users_controller.post_users)
-router.get('/logout',users_controller.get_logout)
-router.get('/update/:idUsuario',users_controller.get_update);
-router.get('/delete/:idUsuario',users_controller.get_delete);
-router.get('/view/:idUsuario',users_controller.get_view);
-router.post('/update/:idUsuario',users_controller.post_update);
-router.get('/search', users_controller.searchUsers);
-=======
 router.get('/',isAuth,usersController.getUsers)
 router.post('/',isAuth,usersController.postUsers)
 router.get('/logout',usersController.getLogout)
@@ -22,7 +12,5 @@ router.get('/update/:idUsuario',usersController.getUpdate);
 router.get('/delete/:idUsuario',usersController.getDelete);
 router.get('/view/:idUsuario',usersController.getView);
 router.post('/update/:idUsuario',usersController.postUpdate);
-
->>>>>>> 9fc9bca5afc901f6f38baed58e1dc361d854517d
-
+router.get('/search',usersController.searchUsers);
 module.exports =  router;
