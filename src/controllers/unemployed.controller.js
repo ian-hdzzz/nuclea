3a2854080797cdc5650c1111c75eaee2af0717c0
Angel D CR <Angel>
2025-04-19 09:11:08 -0600

    Crear model, controller y routes para Unemployed

---
+++ src/controllers/unemployed.controller.js
@@ -0,0 +1,17 @@
+const Usuario = require('../models/unemployed.model');
+
+exports.getUnemployedUsers = (req, res) => {
+  Usuario.fetchUnemployedUsers()
+    .then(([users]) => {
+      res.render('pages/unemployed', {
+        usuarios: users,
+        title: 'Unemployed Users',
+        nombreUsuario: req.session.nombre,
+        apellidosUsuario: req.session.apellidos
+      });
+    })
+    .catch((error) => {
+      console.error('Error al obtener usuarios inactivos:', error);
+      res.status(500).send('Error interno al cargar usuarios inactivos.');
+    });
+};
