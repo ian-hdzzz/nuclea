050c7ccbf425724706f29721f0523bc836fadf3b
SebastianOso <sebastian.oso03@gmail.com>
2025-04-23 11:10:01 -0600

    Arreglando el Dashboard

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -1,12 +1,33 @@
 const Request = require('../models/request.model');
-exports.getDashboard = (req, res) => {
-    Request.fetchDays(req.session.idUsuario)
-    .then(([rows])=>
-        res.render('pages/dashboard',{
-            title:'Dashboard', 
-            iconClass:'fa-solid fa-house', 
-            currentPath: req.path,
-            diasRestantes: rows[0].dias_vaciones
-        }));
+const Holiday = require('../models/holiday.model');
+exports.getDashboard = async (req, res) => {
+    try {
+        const [asueto] = await Holiday.fetchtop3();
+        const [[dias]] = await Request.fetchDays(req.session.idUsuario);
+        const [[pendiente]] = await Request.countPendientesVacaciones(req.session.idUsuario);
 
+        console.log('asueto sin stringify', asueto);
+        console.log('asueto', JSON.stringify(asueto));
+
+        console.log('dias sin stringify', dias);
+        console.log('dias', JSON.stringify(dias));
+
+        console.log('pendientes sin stringify', pendiente);
+        console.log('pendientes', JSON.stringify(pendiente));
+
+
+        res.render('pages/dashboard', {
+            title: 'Dashboard',
+            iconClass: 'fa-solid fa-house',
+            currentPath: req.path,
+            diasRestantes: JSON.stringify(dias.dias_vaciones),
+            asuetos: JSON.stringify(asueto),
+            pendientes: JSON.stringify(pendiente.totalPendientes),
+        });
+    } catch (error) {
+        console.error('Error en getDashboard:', error);
+        if (!res.headersSent) {
+            res.status(500).send('Internal Server Error');
+        }
+    }
 };
