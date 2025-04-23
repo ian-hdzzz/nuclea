801b4e9b3699d8dc9153c3de57ad77dbe5606046
Ian <ianhdez2020@gmail.com>
2025-04-23 12:51:36 -0600

    claves API actualizadaw

--- src/util/passport.js
+++ src/util/passport.js
@@ -42,7 +42,7 @@ passport.use(new LocalStrategy(
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/nuclea/auth/google/callback"
+    callbackURL: "/auth/google/callback"
   },
   async (accessToken, refreshToken, profile, done) => {
     console.log("Google profile:", JSON.stringify(profile));
