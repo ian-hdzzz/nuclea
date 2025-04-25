17c318abf41c5078186f5b97ece5d707d4b04b57
Ian <ianhdez2020@gmail.com>
2025-04-25 00:49:02 -0600

    cambios en el input search

--- src/models/evento.model.js
+++ src/models/evento.model.js
@@ -6,20 +6,26 @@ const Evento = {
         try {
             const [rows] = await db.query(`
                 SELECT 
-                    eventId, 
-                    titulo, 
-                    descripcion, 
-                    fechaInicio, 
-                    horaInicio, 
-                    fechaFin, 
-                    horaFin, 
-                    tipoId, 
-                    usuarioId, 
-                    estado
+                    e.eventoId, 
+                    e.titulo, 
+                    e.descripcion, 
+                    e.fechaInicio, 
+                    e.horaInicio, 
+                    e.fechaFin, 
+                    e.horaFin, 
+                    e.tipoId, 
+                    e.usuarioId, 
+                    e.estado,
+                    e.diaCompleto,
+                    t.nombre as tipoNombre,
+                    t.color as tipoColor
                 FROM 
-                    eventos 
+                    eventos e
+                    LEFT JOIN tiposEvento t ON e.tipoId = t.tipo_id
                 WHERE 
-                    usuarioId = ? AND estado = 'active'
+                    e.usuarioId = ? AND e.estado = 'active'
+                ORDER BY 
+                    e.fechaInicio ASC
             `, [usuarioId]);
             return rows;
         } catch (error) {
@@ -31,20 +37,26 @@ const Evento = {
         try {
             const [rows] = await db.query(`
                 SELECT 
-                    eventId, 
-                    titulo, 
-                    descripcion, 
-                    fechaInicio, 
-                    horaInicio, 
-                    fechaFin, 
-                    horaFin, 
-                    tipoId, 
-                    usuarioId, 
-                    estado
+                    e.eventoId, 
+                    e.titulo, 
+                    e.descripcion, 
+                    e.fechaInicio, 
+                    e.horaInicio, 
+                    e.fechaFin, 
+                    e.horaFin, 
+                    e.tipoId, 
+                    e.usuarioId, 
+                    e.estado,
+                    e.diaCompleto,
+                    t.nombre as tipoNombre,
+                    t.color as tipoColor
                 FROM 
-                    eventos 
+                    eventos e
+                    LEFT JOIN tiposEvento t ON e.tipoId = t.tipo_id
                 WHERE 
-                    estado = 'active'
+                    e.estado = 'active'
+                ORDER BY 
+                    e.fechaInicio ASC
             `);
             return rows;
         } catch (error) {
